26f2012d8d4485b2e6aafcd890d5a302c4457036
Julls <Julls@LAPTOP-85CCCQDU>
2024-10-28 17:42:04 -0700

    Agregar archivos de proyecto.

---
+++ EDDProy/Estructuras No Lineales/frmArboles.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace EDDemo.Estructuras_No_Lineales
+{
+    partial class frmArboles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtArbol = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGrafica = new System.Windows.Forms.Button();
+            this.btnRecorrer = new System.Windows.Forms.Button();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.btnCrearArbol = new System.Windows.Forms.Button();
+            this.txtNodos = new System.Windows.Forms.NumericUpDown();
+            this.lblPostOrden = new System.Windows.Forms.Label();
+            this.lblRecorridoPostOrden = new System.Windows.Forms.Label();
+            this.lblInOrden = new System.Windows.Forms.Label();
+            this.lblPreOrden = new System.Windows.Forms.Label();
+            this.lblRecorridoInOrden = new System.Windows.Forms.Label();
+            this.lblRecorridoPreOrden = new System.Windows.Forms.Label();
+            this.btBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNodos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(147, 38);
+            this.txtDato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(223, 37);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnAgregar.Location = new System.Drawing.Point(121, 80);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(68, 34);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Insertar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // txtArbol
+            // 
+            this.txtArbol.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArbol.Location = new System.Drawing.Point(11, 198);
+            this.txtArbol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtArbol.MaximumSize = new System.Drawing.Size(271, 229);
+            this.txtArbol.Multiline = true;
+            this.txtArbol.Name = "txtArbol";
+            this.txtArbol.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtArbol.Size = new System.Drawing.Size(271, 229);
+            this.txtArbol.TabIndex = 2;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpiar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnLimpiar.Location = new System.Drawing.Point(267, 79);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(68, 34);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnGrafica
+            // 
+            this.btnGrafica.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGrafica.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnGrafica.Location = new System.Drawing.Point(339, 79);
+            this.btnGrafica.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGrafica.Name = "btnGrafica";
+            this.btnGrafica.Size = new System.Drawing.Size(68, 34);
+            this.btnGrafica.TabIndex = 3;
+            this.btnGrafica.Text = "Grafica";
+            this.btnGrafica.UseVisualStyleBackColor = false;
+            this.btnGrafica.Click += new System.EventHandler(this.btnGrafica_Click);
+            // 
+            // btnRecorrer
+            // 
+            this.btnRecorrer.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnRecorrer.Location = new System.Drawing.Point(347, 359);
+            this.btnRecorrer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRecorrer.Name = "btnRecorrer";
+            this.btnRecorrer.Size = new System.Drawing.Size(85, 49);
+            this.btnRecorrer.TabIndex = 4;
+            this.btnRecorrer.Text = "Recorrer";
+            this.btnRecorrer.UseVisualStyleBackColor = false;
+            this.btnRecorrer.Click += new System.EventHandler(this.btnRecorrer_Click);
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos.Location = new System.Drawing.Point(227, 9);
+            this.lblDatos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(61, 25);
+            this.lblDatos.TabIndex = 5;
+            this.lblDatos.Text = "Dato";
+            // 
+            // btnCrearArbol
+            // 
+            this.btnCrearArbol.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnCrearArbol.Location = new System.Drawing.Point(147, 157);
+            this.btnCrearArbol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCrearArbol.Name = "btnCrearArbol";
+            this.btnCrearArbol.Size = new System.Drawing.Size(69, 37);
+            this.btnCrearArbol.TabIndex = 6;
+            this.btnCrearArbol.Text = "Crear Arbol";
+            this.btnCrearArbol.UseVisualStyleBackColor = false;
+            this.btnCrearArbol.Click += new System.EventHandler(this.btnCrearArbol_Click);
+            // 
+            // txtNodos
+            // 
+            this.txtNodos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNodos.Location = new System.Drawing.Point(63, 163);
+            this.txtNodos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNodos.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtNodos.Name = "txtNodos";
+            this.txtNodos.Size = new System.Drawing.Size(60, 24);
+            this.txtNodos.TabIndex = 8;
+            this.txtNodos.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblPostOrden
+            // 
+            this.lblPostOrden.AutoSize = true;
+            this.lblPostOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblPostOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostOrden.Location = new System.Drawing.Point(296, 313);
+            this.lblPostOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPostOrden.Name = "lblPostOrden";
+            this.lblPostOrden.Size = new System.Drawing.Size(84, 18);
+            this.lblPostOrden.TabIndex = 9;
+            this.lblPostOrden.Text = "PostOrden:";
+            // 
+            // lblRecorridoPostOrden
+            // 
+            this.lblRecorridoPostOrden.AutoSize = true;
+            this.lblRecorridoPostOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblRecorridoPostOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecorridoPostOrden.Location = new System.Drawing.Point(404, 313);
+            this.lblRecorridoPostOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRecorridoPostOrden.Name = "lblRecorridoPostOrden";
+            this.lblRecorridoPostOrden.Size = new System.Drawing.Size(16, 18);
+            this.lblRecorridoPostOrden.TabIndex = 10;
+            this.lblRecorridoPostOrden.Text = "..";
+            // 
+            // lblInOrden
+            // 
+            this.lblInOrden.AutoSize = true;
+            this.lblInOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblInOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInOrden.Location = new System.Drawing.Point(306, 289);
+            this.lblInOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInOrden.Name = "lblInOrden";
+            this.lblInOrden.Size = new System.Drawing.Size(64, 18);
+            this.lblInOrden.TabIndex = 11;
+            this.lblInOrden.Text = "InOrden:";
+            // 
+            // lblPreOrden
+            // 
+            this.lblPreOrden.AutoSize = true;
+            this.lblPreOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblPreOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreOrden.Location = new System.Drawing.Point(301, 263);
+            this.lblPreOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPreOrden.Name = "lblPreOrden";
+            this.lblPreOrden.Size = new System.Drawing.Size(76, 18);
+            this.lblPreOrden.TabIndex = 12;
+            this.lblPreOrden.Text = "PreOrden:";
+            // 
+            // lblRecorridoInOrden
+            // 
+            this.lblRecorridoInOrden.AutoSize = true;
+            this.lblRecorridoInOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblRecorridoInOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecorridoInOrden.Location = new System.Drawing.Point(404, 289);
+            this.lblRecorridoInOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRecorridoInOrden.Name = "lblRecorridoInOrden";
+            this.lblRecorridoInOrden.Size = new System.Drawing.Size(16, 18);
+            this.lblRecorridoInOrden.TabIndex = 13;
+            this.lblRecorridoInOrden.Text = "..";
+            // 
+            // lblRecorridoPreOrden
+            // 
+            this.lblRecorridoPreOrden.AutoSize = true;
+            this.lblRecorridoPreOrden.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lblRecorridoPreOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecorridoPreOrden.Location = new System.Drawing.Point(404, 263);
+            this.lblRecorridoPreOrden.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRecorridoPreOrden.Name = "lblRecorridoPreOrden";
+            this.lblRecorridoPreOrden.Size = new System.Drawing.Size(16, 18);
+            this.lblRecorridoPreOrden.TabIndex = 14;
+            this.lblRecorridoPreOrden.Text = "..";
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btBuscar.Location = new System.Drawing.Point(194, 80);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(68, 33);
+            this.btBuscar.TabIndex = 15;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = false;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // frmArboles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(514, 455);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.lblRecorridoPreOrden);
+            this.Controls.Add(this.lblRecorridoInOrden);
+            this.Controls.Add(this.lblPreOrden);
+            this.Controls.Add(this.lblInOrden);
+            this.Controls.Add(this.lblRecorridoPostOrden);
+            this.Controls.Add(this.lblPostOrden);
+            this.Controls.Add(this.txtNodos);
+            this.Controls.Add(this.btnCrearArbol);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.btnRecorrer);
+            this.Controls.Add(this.btnGrafica);
+            this.Controls.Add(this.txtArbol);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtDato);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmArboles";
+            this.Text = "frmArboles";
+            this.Load += new System.EventHandler(this.frmArboles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtNodos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtArbol;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGrafica;
+        private System.Windows.Forms.Button btnRecorrer;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.Button btnCrearArbol;
+        private System.Windows.Forms.NumericUpDown txtNodos;
+        private System.Windows.Forms.Label lblPostOrden;
+        private System.Windows.Forms.Label lblRecorridoPostOrden;
+        private System.Windows.Forms.Label lblInOrden;
+        private System.Windows.Forms.Label lblPreOrden;
+        private System.Windows.Forms.Label lblRecorridoInOrden;
+        private System.Windows.Forms.Label lblRecorridoPreOrden;
+        private System.Windows.Forms.Button btBuscar;
+    }
+}
